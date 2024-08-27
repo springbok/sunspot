ee2c8ff96d3824c34ecbf5d452c1bd7a3a4e088e
etienne <etiennevt@gmail.com>
2024-08-27 15:18:49 +1000

    Rails 7.1.4 fixes

--- sunspot_rails/lib/sunspot/rails/log_subscriber.rb
+++ sunspot_rails/lib/sunspot/rails/log_subscriber.rb
@@ -33,11 +33,11 @@ module Sunspot
         name = '%s (%.1fms)' % ["SOLR Request", event.duration]
 
         # produces: path=select parameters={fq: ["type:Tag"], q: "rossi", fl: "* score", qf: "tag_name_text", defType: "edismax", start: 0, rows: 20}
-        path = color(event.payload[:path], BOLD, bold: true)
+        path = color(event.payload[:path], MODES[:bold], bold: true)
         parameters = event.payload[:parameters].map { |k, v|
           v = "\"#{v}\"" if v.is_a? String
           v = v.to_s.gsub(/\\/,'') # unescape
-          "#{k}: #{color(v, BOLD, bold: true)}"
+          "#{k}: #{color(v, MODES[:bold], bold: true)}"
         }.join(', ')
         request = "path=#{path} parameters={#{parameters}}"
 
